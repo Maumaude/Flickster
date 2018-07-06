c57180167a189d6ec7c8effb96a9cc79ebcac63c
Maumaude <emaude.altema@esih.edu>
2018-07-06 07:09:46 -0500

    First commit

---
+++ app/src/main/java/com/example/altema_emaude/flickster/VideoActivity.java
@@ -0,0 +1,14 @@
+package com.example.altema_emaude.flickster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+
+        String url=" https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+        AsyncHttpClient client = new  AsyncHttpClient();
